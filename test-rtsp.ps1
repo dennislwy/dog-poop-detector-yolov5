39c08942506a76c07cf140b03b51b1867bef1027
Dennis Lee <dennis@gsfcorp.com>
2023-05-24 23:43:15 +0800

    minor update

--- test-rtsp.ps1
+++ test-rtsp.ps1
@@ -1,2 +1,2 @@
 & "venv/Scripts/Activate.ps1"
-python yolov5/detect.py --weights 'yolov5/runs/train/exp2/weights/best.pt' --line-thickness 2 --conf-thres 0.5 --source 'rtsp://your_rtsp_url'
+python yolov5/detect.py --weights 'yolov5/runs/train/exp2/weights/best.pt' --line-thickness 2 --conf-thres 0.5 --nosave --source 'rtsp://your_rtsp_url'
