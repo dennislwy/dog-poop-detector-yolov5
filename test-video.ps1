aee772b0683cdab2794bcfb1cc59bd11df42701a
Dennis Lee <dennis@gsfcorp.com>
2023-05-25 16:16:17 +0800

    renamed data folder to dateset

--- test-video.ps1
+++ test-video.ps1
@@ -1,2 +1,2 @@
 & "venv/Scripts/Activate.ps1"
-python yolov5/detect.py --weights 'yolov5/runs/train/exp2/weights/best.pt' --img 2304 --view-img --source 'data/tests/test.mp4'
+python .\yolov5\detect.py --weights '.\yolov5\runs\train\exp4\weights\best.pt' --line-thickness 2 --conf-thres 0.5 --view-img --source 'dataset/tests/test1.mp4'
